84bbe7859703fa18ed61e027bcc359fcde40cb00
Yasunori Shimura <yasu0710@gmail.com>
2023-12-26 10:53:42 +0900

    Improved prompt(Llama2). Add some params in .env
    * Changed the prompt format to aligh with Llama2 style
    jjjkk
    * Added following parameters in .env
    - STREAMING_OUTPUT
    - CHUNK_SIZE
    - CHUNK_OVERLAP

--- openvino-chatbot-rag-pdf.py
+++ openvino-chatbot-rag-pdf.py
@@ -15,9 +15,12 @@ from langchain.vectorstores        import Chroma
 from optimum.intel.openvino import OVModelForCausalLM
 
 load_dotenv(verbose=True)
+env_source_document  = os.environ['SOURCE_DOCUMENT']
+env_chunk_size       = int(os.environ['CHUNK_SIZE'])
+env_chunk_overlap    = int(os.environ['CHUNK_OVERLAP'])
 env_model_embeddings = os.environ['MODEL_EMBEDDINGS']
 env_vectorstore_path = os.environ['VECTORSTORE_PATH']
-env_source_document  = os.environ['SOURCE_DOCUMENT']
+
 env_cache_dir        = os.environ['CACHE_DIR']
 env_model_vendor     = os.environ['MODEL_VENDOR']
 env_model_name       = os.environ['MODEL_NAME']
@@ -27,6 +30,7 @@ ov_config            = {"PERFORMANCE_HINT":"LATENCY", "NUM_STREAMS":"1", "CACHE_
 
 env_num_max_tokens   = int(os.environ['NUM_MAX_TOKENS'])
 env_rag_chain_type   = os.environ['RAG_CHAIN_TYPE']
+streaming=True if os.environ['STREAMING_OUTPUT'] == "True" else False
 
 
 def read_pdf(pdf_path:str):
@@ -70,14 +74,13 @@ def generate_vectorstore_from_documents(
     del vectorstore
     del embeddings
 
-
 if not os.path.exists(env_vectorstore_path):
     stime = time.time()
     print(f'*** Reading the document ({env_source_document})...', end='', flush=True)
     pdf = read_pdf(env_source_document)
     print(f'{len(pdf)} pages read')
     print(f'*** Splitting the document into chunks')
-    docs = split_text(pdf, 500, 50)
+    docs = split_text(pdf, env_chunk_size, env_chunk_overlap)
     print(f'*** Generating embeddings and registering it to the vectorstore')
     generate_vectorstore_from_documents(docs, env_vectorstore_path, env_model_embeddings)
     etime = time.time()
@@ -121,15 +124,21 @@ ov_model_path = f'./{env_model_name}/{env_model_precision}'
 tokenizer = AutoTokenizer.from_pretrained(model_id, trust_remote_code=True, cache_dir=env_cache_dir)
 model = OVModelForCausalLM.from_pretrained(model_id=ov_model_path, device=env_inference_device, ov_config=ov_config, cache_dir=env_cache_dir)
 
-def generate_rag_prompt(question, vectorstore):
+def generate_rag_prompt(question, vectorstore, bos_token='<s>', verbose=False):
+    B_INST, E_INST = '[INST]', '[/INST]'
+    B_SYS, E_SYS = '<<SYS>>\n', '\n<</SYS>>\n\n'
     reference_documents = vectorstore.similarity_search(question, k=4)
-    prompt = 'Answer the question based only on the following context:\n'
+    prompt  = f'{bos_token}{B_INST} {B_SYS}You are responding to highly technical customers. '
+    prompt += 'Answer the question based only on the following context:\n'
     for ref_doc in reference_documents:
         prompt += ref_doc.page_content.replace('\n', '')
-    prompt += f'\n\nQuestion: {question}'
+    prompt += f'{E_SYS}'
+    prompt += f'Question: {question} {E_INST}'
+    if verbose:
+        print(prompt)
     return prompt
 
-# This class is almost identical to TextStreamer class
+# This class is almost identical to TextStreamer class (I implemented this just for my study purpose)
 class MyStreamer(BaseStreamer):
     def __init__(self, tokenizer, **decode_kwargs):
         super().__init__()
@@ -151,14 +160,14 @@ class MyStreamer(BaseStreamer):
             self.token_cache= []
             self.print_pos = 0
         else:
-            print_text = text[self.print_pos: text.rfind(' ')+1]
+            print_text = text[self.print_pos: text.rfind(' ')+1].rstrip('</s>')
             self.print_pos += len(print_text)
             print(print_text, end='', flush=True)
 
     def end(self):
         if len(self.token_cache) > 0:
             text = self.tokenizer.decode(self.token_cache, **self.decode_kwargs)
-            print_text = text[self.print_pos: ]
+            print_text = text[self.print_pos: ].rstrip('</s>')
             print(print_text)
             self.token_cache = []
             self.print_pos = 0
@@ -166,6 +175,7 @@ class MyStreamer(BaseStreamer):
             print()
             self.token_cache = []
             self.print_pos = 0
+
 
 def run_llm_text_generation(model, prompt, tokenizer, max_new_tokens=140, temperature=0.5, repetition_penalty=1.0, streaming=False):
     tokenizer_kwargs = {"add_special_tokens": False}
@@ -191,14 +201,16 @@ def run_llm_text_generation(model, prompt, tokenizer, max_new_tokens=140, temper
     )
     answer_string = tokenizer.batch_decode(answer_tokens, skip_special_tokens=True)
     answer = answer_string[0]
-    if '\n\nAnswer: ' in answer:
-        answer = answer.split('\n\nAnswer: ')[1]
+    if '\nAnswer: ' in answer:
+        answer = answer.split('\nAnswer: ')[1]
     return answer
-
 
 while True:
     print('Question: ', end='', flush=True)
     question = input()
-    prompt = generate_rag_prompt(question, vectorstore)
-    answer = run_llm_text_generation(model, prompt, tokenizer, streaming=True, temperature=0.1, repetition_penalty=1.2)
-    print()
+    prompt = generate_rag_prompt(question, vectorstore, tokenizer.bos_token)
+    answer = run_llm_text_generation(model, prompt, tokenizer, streaming=streaming, temperature=0.1, repetition_penalty=1.2)
+    if streaming:
+        print()
+    else:
+        print(f'\nAnswer: {answer}')
